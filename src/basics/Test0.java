1990861e9b6191d9af63f05a8c99d03843c7cb67
QSP <QSP@DESKTOP-EVK9GMC>
2020-12-05 09:26:16 +0530

    Launching browser

---
+++ src/basics/Test0.java
@@ -0,0 +1,23 @@
+package basics;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test0 {
+
+	public static void main(String[] args) {
+		/**
+		 * To launch the empty chrome browser
+		 */
+	System.setProperty("webdriver.chrome.driver","./Software/chromeDrive.exe");
+	ChromeDriver driver=new ChromeDriver();
+	
+		/**
+		 * To launch the empty firefox browser
+		 */
+//System.setProperty("webdriver.gecko.driver","./software/geckodriver.exe");
+//FirefoxDriver driver1=new FirefoxDriver();
+	
+	}
+
+}
