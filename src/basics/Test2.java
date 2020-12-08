4eb6c7210779f9a498b56ea45e8bb65adcf3649c
QSP <QSP@DESKTOP-EVK9GMC>
2020-12-08 09:26:27 +0530

    WebDriverClassDiagram

---
+++ src/basics/Test2.java
@@ -0,0 +1,15 @@
+package basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","./software/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+
+	}
+
+}
