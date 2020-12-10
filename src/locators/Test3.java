db7227204f61f6c62faf6cacb04faca67b750e41
QSP <QSP@DESKTOP-EVK9GMC>
2020-12-10 09:29:16 +0530

    CssSelector

---
+++ src/locators/Test3.java
@@ -0,0 +1,17 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+	System.setProperty("webdriver.gecko.driver","./software/geckodriver.exe");
+	WebDriver driver=new FirefoxDriver();
+	driver.get("https://demo.actitime.com/login.do");
+	driver.findElement(By.cssSelector("#username")).sendKeys("admin");
+	driver.findElement(By.cssSelector("input[name='pwd']")).sendKeys("manager");
+	}
+
+}
